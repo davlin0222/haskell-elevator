de0fc1e6116945a1df1282988d0688c518918198
David <davlin@creatorise.com>
2024-06-23 12:50:02 +0200

    Add run tests with hspec

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+import Test.Hspec
+
+specs :: Spec
+specs = do
+  describe "Example Test Suite" $ do
+    it "should add two numbers correctly" $
+      (1 + 1) `shouldBe` 2
+
+    it "should concatenate two strings" $
+      ("hello" ++ " " ++ "world") `shouldBe` "hello world"
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec specs
