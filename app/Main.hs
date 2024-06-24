ecf6b857205dbff7d29cc2c26ab35dd6181f8534
David <davlin@creatorise.com>
2024-06-24 13:09:17 +0200

    Add render elevator floors

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,22 @@
 module Main (main) where
 
+-- import Debug.Trace (traceShow)
+
 data Elevator = Elevator {numOfFloors :: Int}
+
+renderElevator :: Elevator -> String
+renderElevator elevator =
+  concat renderFloors
+    where
+      renderFloors = map renderFloor (reverse [1..numOfFloors elevator])
+      renderFloor floorNumber = unlines [
+          "     |   | " ++ show floorNumber,
+          "_____|   |"
+        ]
 
 main :: IO ()
 main = do
   putStrLn ""
   putStrLn $ renderElevator elevator
   where
-    renderElevator elevator =
-      "NumOfFloors: " ++ (show $ numOfFloors elevator)
     elevator = Elevator {numOfFloors = 3}
