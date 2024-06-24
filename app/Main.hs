0e09980c53ee2a83b1402c6aa3747b8aaa9be86d
David <davlin@creatorise.com>
2024-06-24 13:09:17 +0200

    Add move elevetor to new floor number

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,28 @@ module Main (main) where
 
 data Elevator = Elevator {numOfFloors :: Int, cabFloorPosition :: Int}
 
+main :: IO ()
+main = do
+  putStrLn "Elevator control system starting..."
+  let initialElevator = Elevator { numOfFloors = 3, cabFloorPosition = 1 }
+  controlLoop initialElevator
+
+controlLoop :: Elevator -> IO ()
+controlLoop elevator = do
+  putStrLn $ renderElevator elevator
+  putStrLn "Move elevator to floor number:"
+  newFloorNumber <- readLn :: IO Int
+  let updatedElevator = moveElevator elevator newFloorNumber
+  putStrLn $ "New floor number: " ++ show newFloorNumber
+  putStrLn $ renderElevator updatedElevator
+  controlLoop updatedElevator
+
+moveElevator :: Elevator -> Int -> Elevator
+moveElevator e newFloor
+  | newFloor > numOfFloors e = e { cabFloorPosition = numOfFloors e }
+  | newFloor < 1             = e { cabFloorPosition = 1 }
+  | otherwise                = e { cabFloorPosition = newFloor }
+
 renderElevator :: Elevator -> String
 renderElevator elevator =
     unlines renderFloors
@@ -20,13 +42,3 @@ renderElevator elevator =
             "     |   | " ++ show floorNumber,
             "_____|   | "
           ]
-
-main :: IO ()
-main = do
-  putStrLn ""
-  putStrLn $ renderElevator elevator
-  putStrLn "Move elevetor to floor number:"
-  newFloorNumber <- readLn :: IO Int
-  putStrLn $ "New floor number: " ++ show newFloorNumber
-  where
-    elevator = Elevator {numOfFloors = 3, cabFloorPosition = 3}
