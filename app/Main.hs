cc7c2e4fae3e6bb1365e42dad0e20dff6ed44dd4
David <davlin@creatorise.com>
2024-06-24 13:09:17 +0200

    Add render elevator cab

--- app/Main.hs
+++ app/Main.hs
@@ -2,21 +2,28 @@ module Main (main) where
 
 -- import Debug.Trace (traceShow)
 
-data Elevator = Elevator {numOfFloors :: Int}
+data Elevator = Elevator {numOfFloors :: Int, cabFloorPosition :: Int}
 
 renderElevator :: Elevator -> String
 renderElevator elevator =
     unlines renderFloors
     where
       renderFloors = concatMap renderFloor (reverse [1..numOfFloors elevator])
-      renderFloor floorNumber = [
-          "     |   | " ++ show floorNumber,
-          "_____|   |"
-        ]
+      renderFloor floorNumber
+        | cabFloorPosition elevator == floorNumber || cabFloorPosition elevator + 1 == floorNumber =
+          [
+            "     |   | " ++ show floorNumber,
+            "_____|___| "
+          ]
+        | otherwise =
+          [
+            "     |   | " ++ show floorNumber,
+            "_____|   | "
+          ]
 
 main :: IO ()
 main = do
   putStrLn ""
   putStrLn $ renderElevator elevator
   where
-    elevator = Elevator {numOfFloors = 3}
+    elevator = Elevator {numOfFloors = 3, cabFloorPosition = 2}
