b78091a905dd88406e74bf914c8e0142b0225508
David <davlin@creatorise.com>
2024-06-24 13:09:17 +0200

    Add elevator state as separate data type

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,13 @@ module Main (main) where
 -- import Debug.Trace (traceShow)
 import Text.Read (readMaybe)  -- Import readMaybe explicitly from Text.Read
 
-data Elevator = Elevator {numOfFloors :: Int, cabFloorPosition :: Int}
+data ElevatorState = ElevatorState {cabFloor :: Int}
+data Elevator = Elevator {topFloor :: Int, state :: ElevatorState}
 
 main :: IO ()
 main = do
   putStrLn "Elevator control system starting..."
-  let initialElevator = Elevator { numOfFloors = 3, cabFloorPosition = 1 }
+  let initialElevator = Elevator { topFloor = 3, state = ElevatorState { cabFloor = 1 } }
   controlLoop initialElevator
 
 controlLoop :: Elevator -> IO ()
@@ -24,7 +25,7 @@ controlLoop elevator = do
       putStrLn "Move elevator to floor number:"
       input <- getLine
       case readMaybe input of
-        Just floorNum | (isValidFloor $ numOfFloors elevator) floorNum -> return floorNum
+        Just floorNum | (isValidFloor $ topFloor elevator) floorNum -> return floorNum
         _ -> do
           putStrLn "Invalid input. Please enter a valid floor number."
           promptFloorNumber
@@ -33,24 +34,28 @@ isValidFloor :: Int -> Int -> Bool
 isValidFloor topFloor floorNumber = floorNumber >= 1 && floorNumber <= topFloor
 
 moveElevator :: Elevator -> Int -> Elevator
-moveElevator e newFloor
-  | newFloor > numOfFloors e = e { cabFloorPosition = numOfFloors e }
-  | newFloor < 1             = e { cabFloorPosition = 1 }
-  | otherwise                = e { cabFloorPosition = newFloor }
+moveElevator elevator newFloor
+  | newFloor > topFloor elevator =
+      elevator { state = (state elevator) { cabFloor = topFloor elevator } }
+  | newFloor < 1 =
+      elevator { state = (state elevator) { cabFloor = 1 } }
+  | otherwise =
+      elevator { state = (state elevator) { cabFloor = newFloor } }
+
 
 renderElevator :: Elevator -> String
 renderElevator elevator =
-    unlines renderFloors
-    where
-      renderFloors = concatMap renderFloor (reverse [1..numOfFloors elevator])
-      renderFloor floorNumber
-        | cabFloorPosition elevator == floorNumber || cabFloorPosition elevator + 1 == floorNumber =
-          [
-            "     |   | " ++ show floorNumber,
-            "_____|___| "
-          ]
-        | otherwise =
-          [
-            "     |   | " ++ show floorNumber,
-            "_____|   | "
-          ]
+	unlines renderFloors
+	where
+		renderFloors = concatMap renderFloor (reverse [1..topFloor elevator])
+		renderFloor floorNumber
+			| cabFloor (state elevator) == floorNumber || cabFloor (state elevator) + 1 == floorNumber =
+				[
+					"     |   | " ++ show floorNumber,
+					"_____|___| "
+				]
+			| otherwise =
+				[
+					"     |   | " ++ show floorNumber,
+					"_____|   | "
+				]
